91a62f65eb48f7f5c20217ad1e2d36468d62658d
DieG02 <43993554@alejandrovolta.edu.ar>
2020-07-15 13:55:50 -0300

    estilos

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -26,9 +26,38 @@ export default function LoginForm(){
           login(user) 
         }
       }>
-        <input type="email" name="email" onChange={e => setUser({...user, email: e.target.value})}/>
-        <input type="password" name="password" onChange={e => setUser({...user, password: e.target.value})}/>
-        <input type="submit" value="Enviar"/>
+
+
+        <div className="form-group col-md-12">
+          <h3 style={{paddingBottom: '20px'}}>Log In</h3>
+          <div className="input-group mb-3">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="inputGroup-sizing-default">@</span>
+            </div>
+            <input 
+              type="email" 
+              name="email" 
+              className="form-control" 
+              style={{width: '350px'}}
+              placeholder="Ej: nombreapellido@gmail.com" 
+              onChange={e => setUser({...user, email: e.target.value})}
+              required/>
+          </div>  
+        </div>
+    
+          <div className="form-group col-md-12">
+            <div className="input-group mb-3">
+              <input 
+                type="password"
+                name="password" 
+                className="form-control" 
+                placeholder="Contraseña" 
+                onChange={e => setUser({...user, password: e.target.value})}
+                required/>
+            </div>  
+          </div>
+        
+        <input type="submit"  className="btn btn-danger" value="Enviar"/>
       </form>
     </div>
   )
