8cb467652b90c84eb21196c0f218c67213db0fac
nmx <nahu_m@hotmail.com>
2020-07-17 20:45:01 -0300

    review en proceso

--- client/src/actions/actionsProductos.js
+++ client/src/actions/actionsProductos.js
@@ -4,11 +4,12 @@ export const ADD_PRODUCT = 'ADD_PRODUCT';
 export const EDIT_PRODUCT = 'EDIT_PRODUCT';
 export const DETAIL_PRODUCT = 'DETAIL_PRODUCT';
 export const PRODUCT_BY_CATEGORY = 'PRODUCT_BY_CATEGORY';
+export const ADD_REVIEW = 'ADD_REVIEW';
 
 //Buscar todo
 export function getAll() {
         return function(dispatch) {
-            return fetch(`http://localhost:1337/productos`)
+            return fetch(`http://localhost:1337/productos`, { credentials: 'include' })
             .then(res => res.json())
             .then((data) => {
                 if (data !== undefined) {
@@ -52,29 +53,27 @@ export function editProduct(id, input, category){
         return fetch(`http://localhost:1337/productos/${id}`, {
             method: 'PUT',
             body: JSON.stringify(input), 
-            headers:{
-            'Content-Type': 'application/json'
-        },
-        }).then((res)=>{
-        if(res.status === 200){
-            fetch(`http://localhost:1337/productos/pxcategoria/${id}`, {
-                method: 'PUT',
-                body: JSON.stringify(category), 
-                headers:{
-                'Content-Type': 'application/json'
-            },
-        }).then((res) => {
+            headers:{ 'Content-Type': 'application/json' }
+        })
+        .then(res => {
+            if(category){
+                fetch(`http://localhost:1337/productos/pxcategoria/${id}`, {
+                    method: 'PUT',
+                    body: JSON.stringify(category), 
+                    headers:{ 'Content-Type': 'application/json' }
+                })
+                .then(res => {
+                    if (res.status === 200){
+                        window.location.replace('http://localhost:3000');
+                        return dispatch({ type: EDIT_PRODUCT })
+                    } 
+                })
+            }
             if (res.status === 200){
                 window.location.replace('http://localhost:3000');
                 return dispatch({ type: EDIT_PRODUCT })
-            } else {
-                alert("no se realizo la accion!")
-            }
+            }     
         })
-        } else {
-            alert('No se edito el producto!');
-        }
-    })
     }
 }
 
@@ -99,5 +98,27 @@ export function productByCategory(id){
             dispatch({ type: PRODUCT_BY_CATEGORY, payload: data })
         }
         });
+    }
+}
+
+
+// action para agregar review
+
+export function addReview (idProduct, data){
+    return function (dispatch){
+        return fetch(`http://localhost:1337/review/${idProduct}`, {
+            method: 'POST', 
+            body: JSON.stringify(data), 
+            headers:{
+            'Content-Type': 'application/json'
+            }
+        }).then((res)=>{
+        if(res.status === 200){   
+            dispatch({ type: ADD_REVIEW, payload: data }) 
+            // return window.location.replace('http://localhost:3000')
+        } else {
+            alert('No se agrego la review!');
+        }
+        })
     }
 }
--- client/src/components/ProductDetail.js
+++ client/src/components/ProductDetail.js
@@ -1,44 +1,90 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './ProductDetail.css'
 import { connect } from 'react-redux';
-import { detailProduct } from '../actions/actionsProductos.js';
+import { detailProduct, addReview } from '../actions/actionsProductos.js';
 import { addToCart } from '../actions/actionsCart.js';
+import ReactStars from "react-rating-stars-component";
 
-function ProductDetail ({ id, detailProduct, addToCart, producto }) {
+function ProductDetail ({ id, detailProduct, addToCart, producto, addReview }) {
   
-  useEffect(()=>{
-    detailProduct(id)},
-    [id, detailProduct])
+    const [input, setInput] = useState({});
+
+    const inputChange = function(e){
+      setInput({
+          ...input,
+          [e.target.name] : e.target.value
+      })
+  }
+
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
+
+    useEffect(()=>{
+      detailProduct(id)},
+      [id, detailProduct])
+  
+    useEffect(() => {
+      setInput(producto);
+    }, [producto]);
 
   return (
-    <div className="mi-contenedor">
-      <div className="img-container">
-        <img src={producto.imagen} alt="Imagen en detalle"/>
-      </div>
-      <div className="data-container">
-        <div className="nombre-container">
-          <h2>{producto.nombre}</h2>
-          <p>Vale por una review *****</p>
-        </div>
-        <div className="precio-container">
-          <h4>$ {producto.precio}</h4>
-          <p>Stock disponible {producto.stock}</p>
-        </div>
-        <div className="info-container">
-          <h5>Descripcion del producto</h5>
-          <p> {producto.descripcion || 'Este producto no tiene descripción'} </p>  
-          <div className="color-talle">
-            <span> Colores <div style={{backgroundColor: producto.color, width: '20px', height: '20px'}}></div> </span>
-            <span> Talle {producto.talle} </span>
+    <div>
+      <form onSubmit={ e => {
+            e.preventDefault();
+            addReview(id, input);
+        }}>
+          <div className="mi-contenedor">
+            <div className="img-container">
+              <img src={producto.imagen} alt="Imagen en detalle"/>
+            </div>
+            <div className="data-container">
+              <div className="nombre-container">
+                <h2>{producto.nombre}</h2>
+                <p>Vale por una review
+                  <ReactStars
+                    count={5}
+                    onChange={ratingChanged}
+                    size={24}
+                    isHalf={true}
+                    emptyIcon={<i className="far fa-star"></i>}
+                    halfIcon={<i className="fa fa-star-half-alt"></i>}
+                    fullIcon={<i className="fa fa-star"></i>}
+                    activeColor="#ffd700"
+                  />
+                </p>
+              </div>
+              <div className="form-group">
+                <input 
+                  className="form-control" 
+                  placeholder="Dejá tu comentario (Opcional)" //comentario de la review
+                  onChange={inputChange}
+                />
+                <button type="submit" className="btn btn-primary">Añadir</button>
+              </div>
+
+              <div className="precio-container">
+                <h4>$ {producto.precio}</h4>
+                <p>Stock disponible {producto.stock}</p>
+              </div>
+              <div className="info-container">
+                <h5>Descripcion del producto</h5>
+                <p> {producto.descripcion || 'Este producto no tiene descripción'} </p>  
+                <div className="color-talle">
+                  <span> Colores <div style={{backgroundColor: producto.color, width: '20px', height: '20px'}}></div> </span>
+                  <span> Talle {producto.talle} </span>
+                </div>
+              </div>
+              <div className="btn-container">
+                <button className="btn btn-outline-success" onClick={() => addToCart(id)}>Añadir al carrito</button> 
+              </div>         
+            </div>           
           </div>
-        </div>
-        <div className="btn-container">
-          <button className="btn btn-outline-success" onClick={() => addToCart(id)}>Añadir al carrito</button> 
-        </div>         
-      </div>           
+        </form>
     </div>
   )
 };
+
 
 function mapStateToProps(state){
   return {
