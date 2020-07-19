ee312b01615e08cba507cf3c7d20e972d6eda004
DieG02 <43993554@alejandrovolta.edu.ar>
2020-07-19 13:02:50 -0300

    fixed profile (User) & view profile (Admin)

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -5,7 +5,6 @@ export default function LoginForm(){
   return(
     <div>
       <form action="http://localhost:1337/usuario/login" method="POST">
-
         <div className="form-group col-md-12">
           <h3 style={{paddingBottom: '20px'}}>Log In</h3>
           <div className="input-group mb-3">
@@ -20,8 +19,7 @@ export default function LoginForm(){
               placeholder="henry27"            
               required/>
           </div>  
-        </div>
-    
+        </div>   
           <div className="form-group col-md-12">
             <div className="input-group mb-3">
               <input 
@@ -32,9 +30,11 @@ export default function LoginForm(){
                 required/>
             </div>  
           </div>
-        
+          <div className="form-group form-group-sm col-md-12">
+             <a href="/password/reset">¿Olvidaste la contraseña?</a>
+          </div>
         <input type="submit" className="btn btn-danger" value="Log In"/>
-      </form>
+      </form>   
     </div>
   )
 }
--- client/src/components/Usuario.js
+++ client/src/components/Usuario.js
@@ -1,38 +1,39 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { getUserLogged } from '../actions/actionsUser.js';
+import { getUser, getUserLogged } from '../actions/actionsUser.js';
 import edit from '../images/iconEdit.png';
 import { Form } from 'react-bootstrap';
 import './Usuario.css';
 
-function Usuario ({ logged, id, getUserLogged }) {
+function Usuario ({ usuario, logged, id, getUser, getUserLogged }) {
 
-  useEffect(()=>{getUserLogged()},[id, getUserLogged]);
+  useEffect(() => {getUser(id)}, [id, getUser]);
+  useEffect(() => {getUserLogged()}, [id, getUserLogged]);
   return ( 
     
     <Form>
       <br></br>
       <h2> Datos del Usuario </h2>
       <br></br>
-      <a title="Editar" className="iconleft" href={`http://localhost:3000/usuario/perfil/edit/${logged.id}`}>
+      <a title="Editar" className="iconleft" href={`http://localhost:3000/usuario/perfil/edit/${id}`}>
         <img src={edit} alt="Editar"/>Editar
       </a>
       <fieldset disabled>
             <Form.Group>
               <Form.Label htmlFor="disabledTextInput">Nombre</Form.Label>
-              <Form.Control id="disabledTextInput" value={logged.nombre} />
+              <Form.Control id="disabledTextInput" value={usuario.nombre || logged.nombre } />
             </Form.Group>
             <Form.Group>
               <Form.Label htmlFor="disabledTextInput">Apellido</Form.Label>
-              <Form.Control id="disabledTextInput" value={logged.apellido} />
+              <Form.Control id="disabledTextInput" value={usuario.apellido || logged.apellido} />
             </Form.Group>
             <Form.Group>
               <Form.Label htmlFor="disabledTextInput">Nombre de usuario</Form.Label>
-              <Form.Control id="disabledTextInput" value={logged.nombreusuario} />
+              <Form.Control id="disabledTextInput" value={usuario.nombreusuario || logged.nombreusuario} />
             </Form.Group>
             <Form.Group>
               <Form.Label htmlFor="disabledTextInput">Email</Form.Label>
-              <Form.Control id="disabledTextInput" value={logged.email} />
+              <Form.Control id="disabledTextInput" value={usuario.email || logged.email} />
             </Form.Group>
       </fieldset>
     </Form>
@@ -41,8 +42,9 @@ function Usuario ({ logged, id, getUserLogged }) {
 
 function mapStateToProps(state){
   return {
+      usuario: state.usuario.usuario,
       logged: state.usuario.logged
   }
 }
 
-export default connect(mapStateToProps,{ getUserLogged })(Usuario)
+export default connect(mapStateToProps,{ getUser, getUserLogged })(Usuario)
